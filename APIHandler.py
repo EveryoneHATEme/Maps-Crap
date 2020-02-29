c9fddeb1a20a4e81bc291bd5151708b40ed13fce
EveryoneHATEme <everyonehateme@yandex.ru>
2020-02-29 16:36:03 +0500

    Небольшие фичи с проектированием

--- APIHandler.py
+++ APIHandler.py
@@ -4,24 +4,34 @@ from PIL import Image, ImageQt
 
 
 class RequestHandler:
-    def __init__(self, settings: dict):
-        self.settings = settings
+    def __init__(self):
+        self.settings = {
+            'size': '650,450',
+            'lang': 'ru_RU',
+            'l': 'map',
+            'z': '17',
+            'll': '0.0,0.0'
+        }
         self.static_address = 'http://static-maps.yandex.ru/1.x/'
         self.latitude, self.longitude = None, None
 
-    def get_image_by_coords(self, latitude: float, longitude: float, scale: int):
+    def set_settings(self, **kwargs):
+        if 'size' in kwargs:
+            self.settings['size'] = kwargs['size']
+        if 'lang' in kwargs:
+            self.settings['lang'] = kwargs['lang']
+        if 'l' in kwargs:
+            self.settings['l'] = kwargs['l']
+
+    def set_coords(self, latitude: float, longitude: float):
+        self.latitude, self.longitude = latitude, longitude
+
+    def get_image(self):
         params = {key: value for key, value in self.settings.items() if key in {'l', 'size', 'lang'}}
-        params['ll'] = f'{latitude},{longitude}'
-        params['z'] = f'{scale}'
+        params['ll'] = f'{self.latitude},{self.longitude}'
+        params['z'] = f'{self.scale}'
         response = requests.get(self.static_address, params=params)
         if not response:
             print('no response')
             return
-        self.latitude, self.longitude = latitude, longitude
         return ImageQt.toqpixmap(Image.open(BytesIO(response.content)))
-
-
-if __name__ == '__main__':
-    dct = {'l': 'map', 'size': '650,450', 'lang': 'ru_RU'}
-    handler = RequestHandler(dct)
-    handler.get_image_by_coords(60.597465, 56.838011, 17)
